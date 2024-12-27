c078b57721596146956daee04470fa449c87659a
Camper_Samu <samueleverga@gmail.com>
2024-12-27 18:21:10 +0100

    feat(fabric): add example greet mod
    run by using `gradle :examples:fabric-mod:runServer`

---
+++ examples/fabric-mod/build.gradle.kts
@@ -0,0 +1,45 @@
+buildscript {
+    repositories {
+        maven("https://maven.fabricmc.net/")
+    }
+}
+
+plugins {
+    id("java")
+    id("fabric-loom") version "1.8-SNAPSHOT"
+}
+
+repositories {
+    maven("https://libraries.minecraft.net")
+    maven("https://maven.fabricmc.net/")
+}
+
+val minecraft_version = "1.21"
+val yarn_mappings = "1.21+build.9"
+val loader_version = "0.16.7"
+val fabric_version = "0.102.0+1.21"
+val permissions_version = "0.3.1"
+
+dependencies {
+    implementation(project(":common"))
+    implementation(project(":brigadier"))
+    modImplementation(project(":fabric"))
+    mappings("net.fabricmc:yarn:${yarn_mappings}:v2")
+    minecraft("com.mojang:minecraft:${minecraft_version}")
+    modImplementation("net.fabricmc:fabric-loader:${loader_version}")
+    include("me.lucko:fabric-permissions-api:${permissions_version}")?.let { modImplementation(it) }
+}
+
+java.toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+
+tasks {
+    processResources {
+        filesMatching("fabric.mod.json") {
+            expand(mapOf(
+                "version" to project.version,
+                "loader_version" to loader_version,
+                "minecraft_version" to minecraft_version
+            ))
+        }
+    }
+}
